12bda592e5562216da9d8e4ab7d5adebd5d0dd82
Trilokhw <pranavxyy@gmail.com>
2024-08-16 00:05:29 +0530

    config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/tcy0GLX",
+ALIVE_MSG: process.env.ALIVE_MSG || "hello, i am yuno-md",
 };
