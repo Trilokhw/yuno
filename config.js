1ed70beb656b4a7dd4d3509eac0d1a207242e811
Trilokhw <pranavxyy@gmail.com>
2024-08-17 09:06:31 +0530

    𝐈 𝐊 𝐔 𝐙 𝐄 𝐗 𝐑 🐊

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/tcy0GLX",
-ALIVE_MSG: process.env.ALIVE_MSG || "hello, i am yuno-md",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2147e260c1e68aa62a2f9.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "> *ɪ ᴀᴍ ᴀʟᴡᴀʏs ᴀʟɪᴠᴇ : 🧘🏻*",
 };
