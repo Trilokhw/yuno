853b17ad81b233b2c6392de62fac4a4b5e5aa916
Trilokhw <pranavxyy@gmail.com>
2024-08-17 08:46:43 +0530

    𝐈 𝐊 𝐔 𝐙 𝐄 𝐗 𝐑 🐊

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94779415698']
+const ownerNumber = ['916282589020']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
